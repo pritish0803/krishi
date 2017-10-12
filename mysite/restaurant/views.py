67671af89272ea956c8e24983396bd7b7dd64c0e
pritish gupta <pritish9040246119@gmail.com>
2017-10-12 20:12:08 +0530

    Create views.py

---
+++ mysite/restaurant/views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import random
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Restaurant
+from django.views.generic import TemplateView,ListView
+
+class contentview(TemplateView):
+	
+	template_name="restaurant/content.html"
+class homeview(ListView):
+	
+	#template_name="restaurant/restaurant_view.html"
+	queryset=Restaurant.objects.all()
+	
+class aboutview(TemplateView):
+	
+	template_name="restaurant/about.html"
+class restaurant_view(ListView):
+	#template_name="restaurant.html"
+	def get_queryset(self):
+		slug=self.kwargs.get("slug")
+		if slug:
+			queryset=Restaurant.objects.filter(
+				Q(catagory__iexact=slug)|
+				Q(catagory__icontains=slug)
+		)
+		else:
+			queryset=Restaurant.objects.all()
+		return queryset
