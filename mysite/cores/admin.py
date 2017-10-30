db81d1fcfd007b3c17d171c6a68de889788016e4
pritish gupta <pritish9040246119@gmail.com>
2017-10-30 18:40:51 +0530

    Add files via upload

--- mysite/cores/admin.py
+++ mysite/cores/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Crop,Warehouse,Market,market_crop_price,Transportation
+
+admin.site.register(Crop)
+admin.site.register(Warehouse)
+admin.site.register(Market)
+admin.site.register(Transportation)
+admin.site.register(market_crop_price)
+
--- mysite/cores/models.py
+++ mysite/cores/models.py
@@ -21,3 +21,54 @@ def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+#class Info(models.Model):
+	#user_name=models.ForeignKey(Profile,on_delete=models.CASCADE)
+	
+
+
+
+class Crop(models.Model):
+	crop_name=models.CharField(max_length=30)
+	catagory=models.CharField(max_length=30,help_text='Kharif or Rabi')
+	crop_price_cur=models.IntegerField()
+	crop_price_prev=models.IntegerField(help_text='1 year ago per kg')
+	crop_price_prev2=models.IntegerField(help_text='2 year ago per kg',default=100)
+	crop_price_prev3=models.IntegerField(help_text='3 year ago per kg',default=100)
+	
+	def __unicode__(self):
+		return self.crop_name
+class Warehouse(models.Model):
+	warehouse_name=models.CharField(max_length=30)
+	location=models.CharField(max_length=30)
+	capacity=models.IntegerField()
+	price=models.IntegerField(help_text='monthly price')
+	warehouse_pincode=models.IntegerField(default=751020)
+	def __unicode__(self):
+		return self.warehouse_name
+
+class Market(models.Model):
+	market_name=models.CharField(max_length=30)
+	location=models.CharField(max_length=30)
+	capacity=models.IntegerField()
+	market_pincode=models.IntegerField(default=751020)
+	def __unicode__(self):
+		return self.market_name
+class Transportation(models.Model):
+	transport_company_name=models.CharField(max_length=30)
+	location=models.CharField(max_length=30)
+	capacity_per_person=models.IntegerField(help_text='per person capacity')
+	trip_price_per_km=models.IntegerField(help_text='per km')
+	pincode=models.IntegerField(default=751020)
+	def __unicode__(self):
+		return self.transport_company_name
+class market_crop_price(models.Model):
+	market_id=models.ForeignKey(Market,on_delete=models.CASCADE)
+	crop_id=models.ForeignKey(Crop,on_delete=models.CASCADE)
+	price_per_kg=models.IntegerField()
+	price_per_kg_prev1=models.IntegerField(help_text='1 year ago')
+	price_per_kg_prev2=models.IntegerField(help_text='2 year ago')
+	price_per_kg_prev3=models.IntegerField(help_text='3 year ago')
+
+	def __unicode__(self):
+		return self.price_per_kg
